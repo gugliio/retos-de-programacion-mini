bfedd778b0a98229e9e91005a7469adc8006b378
Ariel Gugliotta <arielgugliotta89@gmail.com>
2024-08-10 19:33:14 -0300

    Reto 05 - IMC - Improvements

--- imc/imc.go
+++ imc/imc.go
@@ -2,11 +2,31 @@ package imc
 
 import (
 	"fmt"
-	"strconv"
 )
+
+/*
+Initial implementation
 
 func Execute(weight float64, height float64) (float64, error) {
 	imc := weight / (height * height)
 	stringRound := fmt.Sprintf("%.1f", imc)
 	return strconv.ParseFloat(stringRound, 64)
+}
+*/
+
+func Execute(weight float64, height float64) string {
+	imc := weight / (height * height)
+	return validateResult(imc)
+}
+
+func validateResult(imc float64) string {
+	if imc < 18.5 {
+		return fmt.Sprintf("IMC: %.1f, Bajo peso", imc)
+	} else if imc >= 18.5 && imc < 25 {
+		return fmt.Sprintf("IMC: %.1f, Normal", imc)
+	} else if imc >= 25 && imc < 30 {
+		return fmt.Sprintf("IMC: %.1f, Sobrepeso", imc)
+	} else {
+		return fmt.Sprintf("IMC: %.1f, Obesidad", imc)
+	}
 }
--- imc/imc_test.go
+++ imc/imc_test.go
@@ -9,30 +9,40 @@ import (
 
 func Test_VowelCounter(t *testing.T) {
 	testTable := []struct {
-		name        string
-		weight      float64
-		height      float64
-		expected    float64
-		expectedErr error
+		name     string
+		weight   float64
+		height   float64
+		expected string
 	}{
+		{
+			name:     "sending weight 65 and height 175 expected 13.1",
+			weight:   40,
+			height:   1.75,
+			expected: "IMC: 13.1, Bajo peso",
+		},
 		{
 			name:     "sending weight 65 and height 175 expected 21,2",
 			weight:   65,
 			height:   1.75,
-			expected: 21.2,
+			expected: "IMC: 21.2, Normal",
 		},
 		{
-			name:     "sending weight 75 and height 165 expected 22,8",
+			name:     "sending weight 75 and height 165 expected 27.5",
 			weight:   75,
 			height:   1.65,
-			expected: 27.5,
+			expected: "IMC: 27.5, Sobrepeso",
+		},
+		{
+			name:     "sending weight 100 and height 165 expected 36.7",
+			weight:   100,
+			height:   1.65,
+			expected: "IMC: 36.7, Obesidad",
 		},
 	}
 	for _, tc := range testTable {
 		t.Run(tc.name, func(t *testing.T) {
-			got, err := imc.Execute(tc.weight, tc.height)
+			got := imc.Execute(tc.weight, tc.height)
 			assert.Equal(t, tc.expected, got)
-			assert.ErrorIs(t, err, tc.expectedErr)
 		})
 	}
 }
